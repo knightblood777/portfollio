780adf82c640deb797af04cc23a20df3a7405b62
knightblood777 <bllllaraf76@gmail.com>
2022-05-13 23:50:41 +0100

    update

--- src/components/uppershapes/uppershape.js
+++ src/components/uppershapes/uppershape.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './uppershapes.css'
+import Typewriter from 'typewriter-effect';
+const author_name = "Charaf eddine eddibaoui"; 
 const Uppershape = () => {
   return (
       <>
@@ -9,9 +11,18 @@ const Uppershape = () => {
   <path fill="#6f42c1" d="M156.4,339.5c31.8-2.5,59.4-26.8,80.2-48.5c28.3-29.5,40.5-47,56.1-85.1c14-34.3,20.7-75.6,2.3-111  c-18.1-34.8-55.7-58-90.4-72.3c-11.7-4.8-24.1-8.8-36.8-11.5l-0.9-0.9l-0.6,0.6c-27.7-5.8-56.6-6-82.4,3c-38.8,13.6-64,48.8-66.8,90.3c-3,43.9,17.8,88.3,33.7,128.8c5.3,13.5,10.4,27.1,14.9,40.9C77.5,309.9,111,343,156.4,339.5z"/>
   </svg>
   </div>
-  <div className="center">Charaf Eddine Eddibaoui</div>
+  <div className="center"><Typewriter
+  onInit={(typewriter) => {
+    typewriter.typeString('<span class="Important">C</span>haraf <span class="Important">E</span>ddine <span class="Important">E</span>ddibaoui')
+      .callFunction(() => {
+        console.log('String typed out!');
+      })
+      .pauseFor(2500)
+      .start();
+  }}
+/></div>
   <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width={0}>
-  <defs>
+  {/* <defs>
 
     
     <filter id="squiggly-0">
@@ -36,9 +47,9 @@ const Uppershape = () => {
       <feTurbulence id="turbulence" baseFrequency="0.02" numOctaves="3" result="noise" seed="4"/>
 <feDisplacementMap in="SourceGraphic" in2="noise" scale="6" />
     </filter>
-  </defs> 
+  </defs>  */}
 </svg>
-    <div className='right'><svg width={315} height={215} id="10015.io" viewBox="0 0 480 480" xmlnsXlink='http://www.w3.org/2000/svg' xmlnsXlink="http://www.w3.org/1999/xlink" >
+    <div className='right'><svg width={315} height={215} id="10015.io" viewBox="0 0 480 480" xmlnsXlink='http://www.w3.org/2000/svg' >
 	<path fill="#6f42c1" d="M390.5,273.5Q367,307,352,344Q337,381,301.5,417Q266,453,219,436.5Q172,420,160,370.5Q148,321,102.5,303Q57,285,56,239.5Q55,194,93.5,169Q132,144,162,129Q192,114,229.5,65Q267,16,314.5,39.5Q362,63,385,107.5Q408,152,411,196Q414,240,390.5,273.5Z" />
 </svg></div>
     </div>
